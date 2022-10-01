29deb4224bd7ced1d9ab565a43827f138b55d795
Inde blockchain official <88079157+web5dev-official@users.noreply.github.com>
2022-10-01 21:47:29 +0530

    up todate

---
+++ staking-dapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className=" bg-blue-400 flex ">
+      <h1 className=' text-center font-[32px]'>website underdevlopment</h1>
+
+    </div>
+  );
+}
+
+export default App;
