f3f6b4cf5a6a24d608a37a5e23203545125b20ad
WEB5DEV <88079157+web5dev-official@users.noreply.github.com>
2022-10-05 12:48:43 +0530

    Update devlopmentico.sol

--- smartcontract/ico/devlopmentico.sol
+++ smartcontract/ico/devlopmentico.sol
@@ -1,13 +1,42 @@
 // SPDX-License-Identifier: MIT
+
+ /*---------------.  .----------------.  .----------------.  .----------------.   .----------------.  .----------------.  .----------------. 
+| .--------------. || .--------------. || .--------------. || .--------------. | | .--------------. || .--------------. || .--------------. |
+| |   ________   | || |  _________   | || |   ______     | || |  ____  ____  | | | |     _____    | || |     ______   | || |     ____     | |
+| |  |  __   _|  | || | |_   ___  |  | || |  |_   __ \   | || | |_  _||_  _| | | | |    |_   _|   | || |   .' ___  |  | || |   .'    `.   | |
+| |  |_/  / /    | || |   | |_  \_|  | || |    | |__) |  | || |   \ \  / /   | | | |      | |     | || |  / .'   \_|  | || |  /  .--.  \  | |
+| |     .'.' _   | || |   |  _|  _   | || |    |  ___/   | || |    > `' <    | | | |      | |     | || |  | |         | || |  | |    | |  | |
+| |   _/ /__/ |  | || |  _| |___/ |  | || |   _| |_      | || |  _/ /'`\ \_  | | | |     _| |_    | || |  \ `.___.'\  | || |  \  `--'  /  | |
+| |  |________|  | || | |_________|  | || |  |_____|     | || | |____||____| | | | |    |_____|   | || |   `._____.'  | || |   `.____.'   | |
+| |              | || |              | || |              | || |              | | | |              | || |              | || |              | |
+| '--------------' || '--------------' || '--------------' || '--------------' | | '--------------' || '--------------' || '--------------' |
+ '----------------'  '----------------'  '----------------'  '----------------'   '----------------'  '----------------'  '----------------' */
+
 pragma solidity ^0.8.0;
- interface ITRC20 {
-    function transfer(address _to, uint256 _value) external returns (bool);
+
+interface IERC20 {
+  
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
 }
+
+pragma solidity ^0.8.4;
+
 
 contract Ownable {
     address private _owner;
 
-    constructor() {
+    constructor () {
         _owner = msg.sender;
     }
 
@@ -25,58 +54,76 @@ contract Ownable {
     }
 }
 
+contract ZEPCOIN_ICO is Ownable{
 
-contract test1 is Ownable{
+    
 
+    
     uint256 public totalRaisedAmount = 0;
     bool public icoStatus;
-    address public contractOwner = 0x9553064692Dcf8189Cf229E5837e89Ac1644AE88;
-    bool public testBool ;
-    address public TokenContract;
-    // mapping (address => uint256) investedAmontinEth;
-    // mapping (address => uint256) ZepcoinAmount;
-        uint256 public current_Price = 1;
+    address public Admin_Wallet;
+    uint256 private current_Price = 10000;
     
    
 
-constructor (address _tokenContract){
-    TokenContract = _tokenContract;
+constructor (address _Admin_Wallet){
+    Admin_Wallet = _Admin_Wallet;
 }
 
-    function ChangePrice(uint256 NewPrice) public onlyOwner{
+    function ChangePrice(uint256 NewPrice) external onlyOwner{
        current_Price = NewPrice;
     }
 
-    function ChangeTokenAddress (address newAddres) external onlyOwner{
-        TokenContract = newAddres;
-    }
 
-    function BuyToken () public {
-        testBool = true;
-        totalRaisedAmount = totalRaisedAmount + 1;
+    function BuyToken_usdt (uint256 Usdt_Amount) public  {
+        require(!icoStatus,"zepcoin ico is stopped");
+        require(Usdt_Amount>=0,"sorry incorect amount");
+        IERC20 usdt = IERC20(
+            address(0x55d398326f99059fF775485246999027B3197955) // usdt Token address 
+        );
+        usdt.transferFrom(msg.sender,Admin_Wallet,Usdt_Amount);
+        IERC20 ZEPX_TK = IERC20(
+            address(0x0Af7aeE626B2641cb1c91c4D42F903D37D88148F) // ZEPX Token address
+        );
+        ZEPX_TK.transfer(msg.sender,Usdt_Amount*current_Price);
+        totalRaisedAmount = totalRaisedAmount + Usdt_Amount;
+
+    }
+    
+    function BuyToken_busd (uint256 busd_Amount) public {
+        require(!icoStatus,"zepcoin ico is stopped");
+        require(busd_Amount>=0,"sorry insufficient balance");
+        IERC20 Busd = IERC20(
+            address(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56) // busd Token address 
+        );
+
+        Busd.transferFrom(msg.sender,Admin_Wallet,busd_Amount);
+        IERC20 ZEPX_TK = IERC20(
+            address(0x0Af7aeE626B2641cb1c91c4D42F903D37D88148F) // ZEPX Token address 
+        );
+        ZEPX_TK.transfer(msg.sender,busd_Amount*current_Price);
+        totalRaisedAmount = totalRaisedAmount + busd_Amount;
 
     }
 
     function StopIco () external onlyOwner {
-        icoStatus = false;
-    }
-
-    function ActivateIco () external onlyOwner {
         icoStatus = true;
     }
 
-   function withdraw_fund(uint256 usdtFund) external onlyOwner {
-        ITRC20 usdt = ITRC20(
-            address(0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684) // testnet addres here
-        );
-        usdt.transfer(0xc40c5393bb0CD04bB2C735f488899463564207e8, usdtFund);
-   }
+    function ActivateIco () external onlyOwner {
+        icoStatus = false;
+    }
 
    function withdraw_unsoldZEPX(uint256 ZEPX_Ammount) public onlyOwner{
-        ITRC20 ZEPX_TK = ITRC20(
-            address(0x7ef95a0FEE0Dd31b22626fA2e10Ee6A223F8a684) // ZEPX Token address here just for testing
+        IERC20 ZEPX_TK = IERC20(
+            address(0x0Af7aeE626B2641cb1c91c4D42F903D37D88148F) // ZEPX Token address 
         );
        ZEPX_TK.transfer(msg.sender,ZEPX_Ammount);
    }
+
+   function change_Admin_Wallet (address _new_Admin) public onlyOwner {
+       Admin_Wallet = _new_Admin;
+   }
     
 }
+
